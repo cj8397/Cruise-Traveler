52ad773694f19d10c5fb62578b063ea8c8e97440
Matthew Catellier <mattcatellier@gmail.coml>
2016-04-20 15:48:08 -0700

    Styled the navbar, and changed layouts a bit

--- project/resources/views/welcome.blade.php
+++ project/resources/views/welcome.blade.php
@@ -15,36 +15,15 @@
     </div>
 </section>
 
-<!-- About Section -->
-<section id="about" class="about-section">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12">
-                <h1>About Section</h1>
-            </div>
-        </div>
-    </div>
-</section>
 
-<!-- Services Section -->
-<section id="services" class="services-section">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12">
-                <h1>Services Section</h1>
-            </div>
-        </div>
-    </div>
-</section>
-
-<!-- Contact Section -->
-<section id="contact" class="contact-section">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12">
-                <h1>Contact Section</h1>
-            </div>
-        </div>
-    </div>
-</section>
+{{--<!-- Contact Section -->--}}
+{{--<section id="contact" class="contact-section">--}}
+    {{--<div class="container">--}}
+        {{--<div class="row">--}}
+            {{--<div class="col-lg-12">--}}
+                {{--<h1>Contact Section</h1>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+{{--</section>--}}
 @endsection
