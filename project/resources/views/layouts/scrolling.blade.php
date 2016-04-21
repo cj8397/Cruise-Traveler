8f6bdbf031e57a26725facb19e8f62bf9619f542
Matthew Catellier <mattcatellier@gmail.coml>
2016-04-21 09:38:16 -0700

    Changed timestamp in nav bar

--- project/resources/views/layouts/scrolling.blade.php
+++ project/resources/views/layouts/scrolling.blade.php
@@ -87,7 +87,7 @@
                         </a>
 
                         <ul class="dropdown-menu" role="menu">
-                            <li><a href="#">Last Update: April 20th, 2016 @ 10:00PM </a> </li>
+                            <li><a href="#">Last Update: April 21th, 2016 @ 9:30AM </a> </li>
                             <li><a href="https://docs.google.com/spreadsheets/d/1KQc5cRAnqdWS55JQb59NHHYKRtqtslBXlHjbWU2QIqc/edit#gid=0">SCRUM WOKRBOOK</a></li>
                             <li><a href="https://docs.google.com/document/d/1yIuRZO1HJ71moInaR_B1Y0mb6yfwxB9oClgWSx5CTpw/edit#heading=h.tphyqzr77ydu">Design Model</a></li>
                         </ul>
