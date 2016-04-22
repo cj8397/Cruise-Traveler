ef1f4b85932eb039428efbf10a5b75ac8fdc72aa
cheng <agilec@icloud.com>
2016-04-22 11:03:15 -0700

    merging - cheng 4/22 11:00

--- project/database/seeds/UsersTableSeeder.php
+++ project/database/seeds/UsersTableSeeder.php
@@ -13,15 +13,25 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->delete();
+
         DB::table('users')->insert([
-            'email' => 'vacationer@gmail.com',
+            'email' => 'vacation@gmail.com',
             'password' => bcrypt('password'),
         ]);
         $faker = Faker::create();
         foreach(range(1,11) as $i) {
             DB::table('users')->insert([
                 'email' => $faker->email,
+                'first' => $faker->firstName,
+                'last' => $faker->lastName,
+                'dob' => $faker->dateTimeBetween($startDate = '-30 years', $endDate = 'now'),
+                'sex' => $faker->boolean($chanceOfGettingTrue = 50),
+                'lang' => $faker->languageCode,
+                'country' => $faker->country,
                 'password' => bcrypt('password'),
+                'created_at' => new DateTime('now'),
+                'updated_at' => new DateTime('now'),
+
             ]);
         }
     }
---
+++ project/resources/views/sailingtest.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.scrolling')
+
+@section('content')
+    @if(isset($success))
+        <div class="alert alert-success">
+            {{$success}}
+        </div>
+    @endif
+
+    @if(isset($failure))
+        <div class="alert alert-danger">
+            {{$failure}}
+        </div>
+    @endif
+    <p>{{$user_id}} {{$sailing_id}} </p>
+    <a href="/joinsailing/{{$user_id}}/{{$sailing_id}}">
+        <button type="button" class="btn btn-primary btn-md">Join</button>
+    </a>
+    <a href="/leavesailing/{{$user_id}}/{{$sailing_id}}">
+        <button type="button" class="btn btn-primary btn-md">Leave</button>
+    </a>
+@endsection
