04408a451282e243dc8140f8b6fff0750a694778
Mario Hugo C Flores <Mariocf_93@hotmail.com>
2016-04-25 11:35:45 -0700

    eventscontroller stuff

--- project/app/Http/Controllers/EventsController.php
+++ project/app/Http/Controllers/EventsController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Sailing;
 use App\User;
+use App\UserEvent;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Event;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use App\Http\Requests\EventRequest;
 use Illuminate\Support\Facades\Response;
@@ -17,8 +19,12 @@ use Illuminate\Support\Facades\Response;
 class EventsController extends Controller
 {
     //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     protected function GetAllParticipantsInEvent($event_id){
-        dd($event_id);
+       return UserEvent::where('event_id', $event_id);
     }
     protected function GetAllEvents($sailing){
         if($events = Event::where('sailing_id', $sailing)){
@@ -30,10 +36,11 @@ class EventsController extends Controller
 
     protected function GetOneEvent($event_id){
         if($event = Event::where('id', $event_id)->first()){
-           // $this->GetAllParticipantsInEvent($event_id);
+            $members = $this->GetAllParticipantsInEvent($event_id);
             $event->start_date = Carbon::parse($event->start_date)->format('l jS \\of F Y h:i:s A');
             $event->end_date = Carbon::parse($event->end_date)->format('l jS \\of F Y h:i:s A');
-            return view('events.eventdetail')->with('event', $event);
+            return view('events.eventdetail')->with(['event' => $event,
+            'members' => $members]);
         }
         else{
             return Redirect::back();
@@ -53,6 +60,11 @@ class EventsController extends Controller
             'end_date' => $end,
             'desc' => $request->desc,
             'location' => $request->location
+        ]);
+        $userevent = UserEvent::create([
+           'user_id' => Auth::user()->id,
+            'event_id' => $event->id,
+            'role' => 'Host'
         ]);
            return redirect()->action('EventsController@GetOneEvent', [$event->id]);
     }
