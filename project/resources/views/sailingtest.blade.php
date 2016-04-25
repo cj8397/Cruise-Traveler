ceb08183c1c44f8fa4ad8e1bfd2efc02783f14b7
Matthew Catellier <mattcatellier@gmail.coml>
2016-04-25 08:32:49 -0700

    Commit to test .gitignore took effect

--- project/resources/views/sailingtest.blade.php
+++ project/resources/views/sailingtest.blade.php
@@ -12,6 +12,7 @@
             {{$failure}}
         </div>
     @endif
+    <p> HEY! GOT RID OF .idea/workspace</p>
     <p>{{$user_id}} {{$sailing_id}} </p>
     <a href="/joinsailing/{{$user_id}}/{{$sailing_id}}"><button type="button" class="btn btn-primary btn-md">Join</button></a>
     <a href="/leavesailing/{{$user_id}}/{{$sailing_id}}"><button type="button" class="btn btn-primary btn-md">Leave</button></a>
