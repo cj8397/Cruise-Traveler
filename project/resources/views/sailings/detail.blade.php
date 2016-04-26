f1fe27f3190e652fb0898ca39390e24f3adb0f77
Jalen Kenny <Jalen@Jalens-Mac-mini.local>
2016-04-25 20:14:50 -0700

    added link from saling detail to saling specific events

--- project/resources/views/sailings/detail.blade.php
+++ project/resources/views/sailings/detail.blade.php
@@ -5,10 +5,8 @@
         <div class="row">
             <div class="col-md-6 col-md-offset-3">
                 <h2>{{$sailing->id}} {{$sailing->cruise_line}} {{$sailing->title}}</h2>
-                <a href="{{ action('SailingsController@GetSailing', [$sailing->id]) }}">
-                    <img class="img-responsive" src="/images/sailingInfo.png" alt="" width="500" height="700">
                 <a href="{{ action('EventsController@GetAllEvents', [$sailing->id]) }}">
-                    <img class="img-responsive" src="/images/sailingInfo.png" alt="" width="500" height="700">
+                    <img class="img-responsive" src="/images/sailingInfo.png" alt="">
                 </a>
                 <a href="/joinsailing/{{$sailing->id}}">
                     <button type="button" class="btn btn-primary btn-md">
