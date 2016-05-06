1fbd11ad3f662a68a006a8a227ff3c966f70f657
Cheng <agilec@gmail.com>
2016-05-06 16:18:50 -0700

    changed time  - cheng 5/6 16:17

---
+++ project/vendor/laracasts/flash/src/Laracasts/Flash/SessionStore.php
@@ -0,0 +1,14 @@
+<?php namespace Laracasts\Flash;
+
+interface SessionStore
+{
+
+    /**
+     * Flash a message to the session.
+     *
+     * @param $name
+     * @param $data
+     */
+    public function flash($name, $data);
+
+} 
