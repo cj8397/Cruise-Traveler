1c4bc2a3a4d9507a7215e889927005d25d473b62
Matthew Catellier <mattcatellier@gmail.coml>
2016-05-10 15:14:44 -0700

    Seeded 15 events for each sailing from frankies data, still a little strange, need to workout some kinks

--- project/app/Http/routes.php
+++ project/app/Http/routes.php
@@ -82,5 +82,24 @@ Route::get('/eventusers/{event_id}', 'UserEventsController@GetAllUsers');
 Route::get('/userevents', 'UserEventsController@GetAllEvents');
 
 Route::get('/test', function() {
-    return View::make('images');
+    $string = 'Slot Tournament - Casino Royale,13:00,23:59,,Casino Royale (Deck 6),
+Diamond & Gemstone Seminar,13:30,14:00,,Deck 13,
+Ice Carving Demonstration,15:00,16:00,,Poolside (Deck 11),
+Pathway To Yoga,17:00,18:00,,ShipShape Center (Deck 12),
+Big Band Dance Music,17:30,18:30,,Colony Club (Deck 6),
+Cigar Aficionados,22:00,23:59,,Sky Bar (Deck 12),
+Singles Mix Mingle,20:00,23:00,,Centrum,
+PHOTO TIME: ELEGANT,16:00,18:00,"It�۪s not often you dress up in your finest digs. Let us capture the moment - give us your best smile and say ���cheese!�۝",Promenade & Lobby,
+KARAOKE AWESOME,21:00,23:59,"Maybe it�۪s a song you choos maybe not. The bottom line is you feel the music and let it take you over.","Alfred�۪s, 4 Mid",
+PARTY LIKE IT�۪S 1980,22:00,23:00,"Hop on the flashback train for a good ol�۪ fashioned 80۪s party. Break out those parachute pants big hair and anything else from the 80�۪s and let�۪s rock!","Henri�۪s, 5 Mid",
+Sudoku and Brainteasers,8:00,11:00,,"Library, Deck 7 Aft",
+Detox for Health and Weight Loss,11:00,13:00,Discover the best way to get rid of vour stubborn belly fat,"Fitness Center, Deck 17 Aft",
+Faclal Workshop: Ladies Pamper Party,13:30,15:00,calling all ladies to attend the pampering event of the cruise. Join us for skin specific or luxury skin knowledqe advice tips and free samples,"Lotus Spa, Deck 5 Fwd",
+Chocolate Journeys Pastry Demonstration,14:00,15:00,,Deck 7,';
+
+    $data = str_getcsv($string, "\n");
+    foreach($data as $row) {
+        $eventInfo = explode(",", $row);
+        var_dump($eventInfo[4]);
+    }
 });
