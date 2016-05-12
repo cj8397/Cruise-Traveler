0d59be3c425b15c29db04485281cad8dec7307aa
Jalen Kenny <Jalen@jalens-mac-mini.wifi.bcit.ca>
2016-05-12 12:03:19 -0700

    fixed register validation

--- project/resources/views/auth/login.blade.php
+++ project/resources/views/auth/login.blade.php
@@ -7,14 +7,14 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Login</div>
                 <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
+                    <form id="loginPageForm" class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
                         {!! csrf_field() !!}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                             <label class="col-md-4 control-label">E-Mail Address</label>
 
                             <div class="col-md-6">
-                                <input type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                                <input type="email" class="form-control" name="email" value="{{ old('email') }}">
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
@@ -28,7 +28,7 @@
                             <label class="col-md-4 control-label">Password</label>
 
                             <div class="col-md-6">
-                                <input type="password" class="form-control" name="password" required>
+                                <input type="password" class="form-control" name="password">
 
                                 @if ($errors->has('password'))
                                     <span class="help-block">
@@ -64,4 +64,7 @@
     </div>
 
 </div>
+@endsection
+@section('scripts')
+{!! JsValidator::formRequest('App\Http\Requests\LoginRequest', '#loginPageForm'); !!}
 @endsection
